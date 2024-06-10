7fba88392676c40313ce788779b234a434bb376b
vikask0199 <vikasku0199@gmail.com>
2024-06-10 14:26:07 +0530

    hash router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,18 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import "../index.css"
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import "../index.css"
+import App from './App.tsx'
 import { store } from './app/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </>,
 )
