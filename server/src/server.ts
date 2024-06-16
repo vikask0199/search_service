262bccbea0a531f74a8e80beb4c24dfe8e06a37a
vikask0199 <vikasku0199@gmail.com>
2024-06-17 04:29:28 +0530

    public

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,33 @@ import dotenv from "dotenv";
 import http from "http";
 import mongoose from "mongoose";
 import app from "./app";
+import fs from "fs";
+import path from "path";
+
+
 
 dotenv.config({ path: ".env" });
 
 const port = 5000;
 const server = http.createServer(app);
+
+const createDirectoryIfNotExists = (directoryPath: string) => {
+  if (!fs.existsSync(directoryPath)) {
+    fs.mkdirSync(directoryPath);
+    console.log(`Created directory: ${directoryPath}`);
+  }
+};
+
+const createPublicTempDirectories = () => {
+  const projectRoot = path.join(__dirname, '..');
+  const publicDir = path.join(projectRoot, "public");
+  const tempDir = path.join(publicDir, "temp");
+
+  createDirectoryIfNotExists(publicDir);
+  createDirectoryIfNotExists(tempDir);
+};
+
+createPublicTempDirectories();
 
 process.on("uncaughtException", (error) => {
   process.exit(1);
